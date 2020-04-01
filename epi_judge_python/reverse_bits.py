1d87017279371a91890792179697d6a8324188d9
Murat Göksel <murat.goxel@gmail.com>
2020-03-31 23:42:04 -0400

    Reverse bits in O(N/L) time

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -1,9 +1,34 @@
 from test_framework import generic_test
 
+PRECOMPUTED_REVERSE = {}
 
+def swap_bits(x, i, j):
+    if (x>>i) & 1 != (x>>j) & 1:
+        x ^= (1 << i) | (1 << j)
+    return x
+
+max_size = 16   # max size for precomputed values
+for num in range(1 << max_size):
+    i, j, rev = 0, max_size-1, num
+    while i < j:
+        rev = swap_bits(rev, i, j)
+        i, j = i+1, j-1
+    PRECOMPUTED_REVERSE[num] = rev
+
+# O(N/L) time where N = the size of x, L = max size of cache key
+# For this implementation N = 64, L = 16; therefore O(1)
 def reverse_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    """
+    x is 64 bit integer. Divide it into 4 parts of 16 bits: x3, x2, x1, x0. 
+    Let y = reverse_bits(x) = f(x). Then y would be f(x0), f(x1), f(x2), and
+    f(x3) ORed together.
+    """
+    mask = 0xFFFF
+    y3 = PRECOMPUTED_REVERSE[x & mask]
+    y2 = PRECOMPUTED_REVERSE[(x >> 16) & mask]
+    y1 = PRECOMPUTED_REVERSE[(x >> 32) & mask]
+    y0 = PRECOMPUTED_REVERSE[x >> 48]
+    return (y3<<48) | (y2<<32) | (y1<<16) | y0
 
 
 if __name__ == '__main__':
