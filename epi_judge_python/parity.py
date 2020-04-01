f08516eaf6baf4a85809bc874aa5c244023be1a2
Murat Göksel <murat.goxel@gmail.com>
2020-03-31 20:53:53 -0400

    Compute parity in less than O(N)

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,10 +1,21 @@
 from test_framework import generic_test
 
 
-def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+# brute force O(N) time in the number of bits of x
+# def parity(x: int) -> int:
+#     result = 0
+#     while x:
+#         result ^= x & 1     # equivalent to  result = (result + x&1) % 2
+#         x >>= 1
+#     return result
 
+# O(K) time in the number of set bits in x, K < N
+def parity(x: int) -> int:
+    result = 0
+    while x:
+        result ^= 1
+        x &= x-1
+    return result
 
 if __name__ == '__main__':
     exit(generic_test.generic_test_main('parity.py', 'parity.tsv', parity))
