41f1d00a36d36ff398df072d20cee09e45d623cf
Murat Göksel <murat.goxel@gmail.com>
2020-03-31 21:31:43 -0400

    Swap bits in O(1) time

--- epi_judge_python/swap_bits.py
+++ epi_judge_python/swap_bits.py
@@ -1,9 +1,20 @@
 from test_framework import generic_test
 
 
-def swap_bits(x, i, j):
-    # TODO - you fill in here.
-    return 0
+def swap_bits(x: int, i: int, j: int) -> int:
+    """
+    Only swap if ith bit is 0 and jth bit is 1 or vice versa.
+    If the bit is set, AND it with ~(1<<i); else, OR it with 1<<i.
+    """
+    bit_i, bit_j  = (x>>i) & 1, (x>>j) & 1
+    if bit_i and not bit_j:
+        x = x & ~(1<<i)
+        x = x |  (1<<j)
+    elif bit_j and not bit_i:
+        x = x & ~(1<<j)
+        x = x |  (1<<i)
+    return x
+    
 
 
 if __name__ == '__main__':
